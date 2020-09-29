55517ab30b9ab99c18a0c54757449e45525aacd2
Martynas <martynascibulskis6@gmail.com>
2020-09-30 01:31:20 +0300

    added basic entity wrapper classes

--- Gariunai Cloud Services/Program.cs
+++ Gariunai Cloud Services/Program.cs
@@ -14,10 +14,28 @@ namespace Gariunai_Cloud_Services
         [STAThread]
         static void Main()
         {
+
+            List<Gariunai_Cloud_Services.Entities.Business> res = DatabaseHelper.GetBusinesses();
+            System.Diagnostics.Debug.WriteLine(res.Count());
+
+            foreach (var shop in DatabaseHelper.GetBusinesses()) 
+            {
+                System.Diagnostics.Debug.WriteLine("-------------------------");
+                System.Diagnostics.Debug.WriteLine("shop name = " + shop.Name);
+                System.Diagnostics.Debug.WriteLine("shop description  = " + shop.Description);
+                System.Diagnostics.Debug.WriteLine("Owner name = " + shop.Owner.Name);
+                System.Diagnostics.Debug.Write("Products = ");
+                foreach(var product in shop.Produce) 
+                {
+                    System.Diagnostics.Debug.Write(product + " ");
+                }
+                System.Diagnostics.Debug.Write("\n");
+            }
+/*
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LocalProduceForm());
+            Application.Run(new LocalProduceForm());*/
         }
     }
 }
