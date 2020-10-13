e8fb8be687e449a5b739d5d30ce2920e0e4424d7
Savickas Tautvilis <tautvilis.savickas@mif.stud.vu.lt>
2020-10-14 02:52:36 +0300

    Added specific shop form
    CHANGED: Log In form tab index, because it was annoying, that it focused
    on password first...

---
+++ Gariunai Cloud Services/SpecificShop/SpecificShopForm.cs
@@ -0,0 +1,40 @@
+﻿using Gariunai_Cloud_Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gariunai_Cloud_Services
+{
+    public partial class SpecificShopForm : Form
+    {
+        private Form _previousForm;
+
+        public SpecificShopForm(Form previousForm, Shop shop)
+        {
+            InitializeComponent();
+            _previousForm = previousForm;
+            shopDescription.Text = shop.Description;
+            //pictureBox1.Image = shop.Image;
+            foreach (var produce in shop.Produce)
+            {
+                productList.Items.Add(produce.Name);
+            }
+        }
+
+        private void SpecificShopForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hide();
+            _previousForm.Show();
+        }
+
+        private void homeBtn_Click(object sender, EventArgs e)
+        {
+            Hide();
+            _previousForm.Show();
+        }
+    }
+}
