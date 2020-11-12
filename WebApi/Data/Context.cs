e33737edbf67c04778cabe949a3fed704c0a66a5
simazm8 <s.pruselaitis@gmail.com>
2020-11-12 12:54:35 +0200

    Added Api

---
+++ WebApi/Data/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using WebApi.Entities;
+
+namespace WebApi.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Notification> Notifications { get; set; }
+
+        public DbSet<Follow> Follows { get; set; }
+
+        public DbSet<Password> Passwords { get; set; }
+
+        public DbSet<Shop> Shops { get; set; }
+
+        public DbSet<Produce> Produce { get; set; }
+    }
+}
---
+++ WebApi/Entities/Produce.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Entities
+{
+    public class Produce
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
