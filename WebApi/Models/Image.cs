d2bd5149a4bdb7747d038e428469bf7e0a94efeb
Martynas <martynascibulskis6@gmail.com>
2020-12-15 22:21:08 +0200

    updated db with image table

---
+++ WebApi/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class Image
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public byte[] Data { get; set; }
+        public string Suffix { get; set; }
+    }
+}
